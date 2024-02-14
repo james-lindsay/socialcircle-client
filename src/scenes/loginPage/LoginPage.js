0fafa14431180080658b45dd8c92576a3da25960
james <jameslindsay2220@gmail.com>
2024-02-14 15:13:32 +0000

    added login page code

--- src/scenes/loginPage/LoginPage.js
+++ src/scenes/loginPage/LoginPage.js
@@ -1,5 +1,36 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
-  return <div>HomePage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 100px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Social Circle
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.color.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Social Circle
+        </Typography>
+        <Form></Form>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
